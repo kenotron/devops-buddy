32f3de5297e9c085dd2aad0248d1a752720577bd
Ken Chau <kchau@microsoft.com>
2024-06-20 14:46:14 -0700

    hides the dependency behind platform check

--- lage.config.js
+++ lage.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   pipeline: {
-    build: ['^build'],
-  }
-}
+    build: ["^build"],
+    bundle: [],
+  },
+};
--- packages/ado-pat/src/cache/createCacheStore.ts
+++ packages/ado-pat/src/cache/createCacheStore.ts
@@ -1,17 +1,19 @@
 import os from "os";
 import path from "path";
-import { WindowsCacheStore } from "./WindowsCacheStore";
-import { KeyringCacheStore } from "./KeyringCacheStore";
 
-export function createCacheStore() {
+import { type ICacheStore } from "./ICacheStore";
+
+export function createCacheStore(): ICacheStore {
   const homeDir = os.homedir();
   const accountName = os.userInfo().username;
   const cacheName = "ado-pats";
   const cachePath = path.join(homeDir, `ado-pat`, ".pats.json");
 
   if (process.platform === "win32") {
+    const { WindowsCacheStore } = require("./WindowsCacheStore");
     return new WindowsCacheStore(cachePath, "CurrentUser", null);
   } else {
+    const { KeyringCacheStore } = require("./KeyringCacheStore");
     return new KeyringCacheStore(cacheName, accountName);
   }
 }
